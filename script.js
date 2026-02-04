226da7051d1ea2ae6f0765d37e0beb34eb122a9f
Doyun <kimdoyunnn@gmail.com>
2026-02-04 05:12:52 +0000

    Theme_v1

--- script.js
+++ script.js
@@ -14,15 +14,17 @@ function draw() {
 
     Object.values(list).map((item, index) => {
         fill(item.color);
-        rect(50, index * 50 + 70, item.amount / 180 * 340, 15);
+        stroke(255);
+        rect(50, index * 60 + 70, item.amount / 180 * 340, 15);
         textAlign("left");
-        fill(0)
-        textSize(12);
-        text(item.name, 50, index * 50 + 65);
+        fill(255)
+        noStroke();
+        textSize(20);
+        text(item.name, 50, index * 60 + 65);
 
-        textSize(22);
+        textSize(32);
         textAlign("right");
-        text(item.icon, 45, index * 50 + 75);
+        text(item.icon, 45, index * 60 + 75);
 
 
     });
